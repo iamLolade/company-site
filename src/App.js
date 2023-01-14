a1ca3597f0630de9e1d318d759532279222711b8
Ololade Otemade <adeniyiotemade@gmail.com>
2023-01-14 13:32:55 +0100

    Home & About component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Brand Name</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
